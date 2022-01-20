377cbc6cf6ab6d0a30ade42e564db12a0d22616a
Diego Castro <DaCastro@outlook.com>
2022-01-20 19:17:26 +0100

    Initial Commit

---
+++ ARWeather/ARWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARWeatherApp.swift
+//  ARWeather
+//
+//  Created by Diego Castro on 20/01/22.
+//
+
+import SwiftUI
+
+@main
+struct ARWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ARWeather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ARWeather
+//
+//  Created by Diego Castro on 20/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
